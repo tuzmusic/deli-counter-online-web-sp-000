ac2037c99c9ce946cb0d66ec9a74a724845bfe9a
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 14:37:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,5 +11,5 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{katz_deli.size + 2} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
 end
