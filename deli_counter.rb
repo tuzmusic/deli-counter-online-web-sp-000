9ec47f85e034ec282011696d8a0db0791779a218
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 14:29:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    str = "The line is currently: "
+    str = "The line is currently:"
     katz_deli.each_with_index do |name, num|
-      str << "#{num+1}. #{name} "
+      str << " #{num+1}. #{name}"
     end
     puts str
   end
